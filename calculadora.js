dccd01d72687f63a570f09526d877d12060541b5
Dany Aguilera <Aguilera@Daniels-MacBook-Pro.local>
2018-01-29 10:01:34 -0700

    se agrega practica de calculadora

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+var args = process.argv.slice(2);
+var num1 = args[0]
+var operator = args[1]
+var num2 = args[2]
+if(args.length==3){
+    switch(operator){
+        case 'mas':
+            console.log("Suma: "+(Number(num1) + Number(num2)));
+            break;
+        case 'menos':
+            console.log("Resta: "+(Number(num1) - Number(num2)));
+            break;
+        case 'por':
+            console.log("Multiplicación: "+(Number(num1) * Number(num2)));
+            break;
+        case 'entre':
+            console.log("División: "+(Number(num1) / Number(num2)));
+            break;
+    }
+}else{
+    console.log("Introduce bien los parámetros");
+}
