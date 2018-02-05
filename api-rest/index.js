b8715c85901dbd31ab786e9ee1be86275a34aa3d
Dany Aguilera <Aguilera@Daniels-MacBook-Pro.local>
2018-02-05 13:10:32 -0700

    practica crear un servidor con nodejs y express

---
+++ api-rest/index.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+
+var server = app.listen(3678, function(){
+   console.log("El servidor esta corriendo");
+});
+
+/* callback */
+//una funcion de callback, es una funcion que es pasada a otra funcion como parametro, y la funcion callback is ejecutada dentro de la funcion que lo llamo como parametro.
+
+/* ES6 */
+//ECMAScript 6 es la nueva version de javascript
+
+/* ES6 Palabras reservadas */
+//const significa que el identificador no puede volver a ser asignado
+// let cuando se necesita reasignar la variable normalmente para ciclos, solo usada en el bloque que esta definido
+// var la variable puede o no ser reasignada y usada, debe ser la menos usada ahora.
